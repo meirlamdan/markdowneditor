3375f2f5e5ca02786449fefd28b524bfda57d0f8
sendmithunb@gmail.com <sendmithunb@gmail.com>
2021-05-01 23:06:33 -0500

    add basic functionality

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="App">
+    <button id="bold" @click="handleBold">Bold</button>
+    <div id="editorWindow">
+      <textarea id="editor" v-model="inputText"></textarea>
+    </div>
+    <div id="displayWindow">
+      <div id="display" v-html="stylizedMarkdown"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import marked from "marked";
+import DOMPurify from "dompurify";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: {},
+  data: () => {
+    return {
+      inputText: "# This is an h1",
+    };
+  },
+  computed: {
+    stylizedMarkdown() {
+      let output = DOMPurify.sanitize(marked(this.inputText));
+      console.log(output);
+      return output;
+    },
+  },
+  methods: {
+    handleBold() {
+      this.inputText = this.inputText + "** ";
+    },
   },
 };
 </script>
@@ -19,8 +42,35 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  #App {
+    display: flex;
+  }
+
+  #editorWindow,
+  #displayWindow {
+    width: 50%;
+    height: 100vh;
+  }
+
+  #displayWindow {
+    background-color: #f5f5f5;
+    padding-left: 1rem;
+    border-radius: 2%;
+  }
+
+  #editorWindow {
+    margin-right: 1rem;
+
+    #editor {
+      border: none;
+      height: 100vh;
+      width: 99%;
+      resize: none;
+      font-size: 1.25rem;
+      border-radius: 2%;
+    }
+  }
 }
 </style>
